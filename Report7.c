98f287347b868912644b91493b992aa967668182
Yuri Kuniyoshi <e235750@ie.u-ryukyu.ac.jp>
2024-01-23 11:00:11 +0900

    同じ数字がある場合も並び替えできるように修正

--- Report7.c
+++ Report7.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <limits.h>
 
-void printArray(int array[], int size);
 void numOrder(int array[], int size);
-int contain(int target, int array[], int size);
+void printArray(int array[], int size);
 
-
-int main(void){
-    const int SIZE = 5; //定数として配列のサイズを宣言
-    int scores[5] = {0, 60, 70, 100, 90};
+int main(void) {
+    const int SIZE = 6;
+    int scores[6] = {100, 60, 70, 100, 90, 80};
     numOrder(scores, SIZE);
 
     printArray(scores, SIZE);
@@ -16,38 +14,21 @@ int main(void){
     return 0;
 }
 
-void printArray(int array[], int size){
-    for(int i = 0; i < size; i ++){
-        printf("%d\n", array[i]);
-    }
-}
-
-void numOrder(int array[], int size){
-    int saved_array[size];
-
-    for(int i = 0; i < size; i ++){
-        int max_value = INT_MIN;
-
-        for(int j = 0; j < size; j ++){
-            if(array[j] >= max_value && !contain(array[j], saved_array, i)){
-                max_value = array[j];
+void numOrder(int array[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            // 同じ数字の場合も交換するように修正
+            if (array[j] < array[j + 1]) {
+                int value = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = value;
             }
         }
-        
-        saved_array[i] = max_value;
-        // printf("%d\n", saved_array[i]);
-    }
-    
-    for(int k = 0; k < size; k ++){
-        array[k] = saved_array[k];
     }
 }
 
-int contain(int target, int array[], int size){
-    for(int i = 0; i < size; i ++){
-        if(target == array[i]){
-            return 1;
-        }
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", array[i]);
     }
-    return 0;
 }
